0ae50724026c2f87846ccb719ec7c969a62b438c
Gino Benitez <benitezgp81@gmail.com>
2024-02-01 15:41:31 -0500

    Missing email from creating new contact

--- POODSD/Signed-In/script.js
+++ POODSD/Signed-In/script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const fName = document.querySelector('input[type="FirstName"]');
     const lName = document.querySelector('input[type="LastName"]');
     const pNumber = document.querySelector('input[type="PhoneNumber"]');
+    const email = document.querySelector('input[type="Email"]');
     const listDisplay = document.getElementById('contactList');
 
     const addCButton = document.querySelector('.addC');
@@ -20,22 +21,29 @@ document.addEventListener('DOMContentLoaded', function() {
         lNameSpan.textContent = lName.value;
         const pNumberSpan = document.createElement('span');
         pNumberSpan.textContent = pNumber.value;
+        const emailSpan = document.createElement('span');
+        emailSpan.textContent = email.value;
+
 
         fNameSpan.innerHTML = "<span class='label'> First Name: </span>" + fName.value;
         lNameSpan.innerHTML = "<span class='label'> Last Name: </span>" + lName.value;
         pNumberSpan.innerHTML = "<span class='label'> Phone Number: </span>" + pNumber.value;
+        emailSpan.innerHTML = "<span class='label'> Email: </span>" + email.value;
 
         newEntry.appendChild(fNameSpan);
         newEntry.appendChild(document.createTextNode(" "));
         newEntry.appendChild(lNameSpan);
         newEntry.appendChild(document.createTextNode(" "));
         newEntry.appendChild(pNumberSpan);
+        newEntry.appendChild(document.createTextNode(" "));
+        newEntry.appendChild(emailSpan);
 
         listDisplay.appendChild(newEntry);
 
         fName.value = '';
         lName.value = '';
         pNumber.value = '';
+        email.value = '';
     });
 
     addCButton.addEventListener('click', function() {
