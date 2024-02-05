c7d7f30028679e9f244d812a848dbb02ad587a34
Gino Benitez <benitezgp81@gmail.com>
2024-02-04 19:57:36 -0500

    Update code.js

--- POODSD/Signed-In/code.js
+++ POODSD/Signed-In/code.js
@@ -205,10 +205,10 @@ function updateContact(id){
     const phone = document.getElementById("phonenumber" + id).value;
     const ID = ids[id];
 
-	document.getElementById("FirstName" + ID).innerHTML = "<span  class='label'> First Name: </span> <p id = 'FirstName"+id+"'>" + name+"</p>";
-    document.getElementById("LastName" + ID).innerHTML = "<span  class='label'> Last Name: </span> <p id = 'LastName"+id+"'>" + last+"</p>";
-    document.getElementById("EmailAddress" + ID).innerHTML = "<span  class='label'> Email Address: </span> <p id = 'EmailAdress"+id+"'>" + email+"</p>";
-    document.getElementById("PhoneNumber" + ID).innerHTML = "<span  class='label'> Phone Number: </span> <p id = 'PhoneNumber"+id+"'>" + phone+"</p>";
+	document.getElementById("FirstName" + id).innerHTML = name;//.value;
+    document.getElementById("LastName" + id).innerHTML = last;//.value;
+    document.getElementById("EmailAddress" + id).innerHTML = email;//.value;
+    document.getElementById("PhoneNumber" + id).innerHTML = phone;//.value;
 
 	let tmp = {
 		"ID": ID,
