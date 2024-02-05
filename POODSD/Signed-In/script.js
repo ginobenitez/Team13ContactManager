2c3e0bcf07e246d29fdb3621154bd3f58395cb49
Gino Benitez <benitezgp81@gmail.com>
2024-02-04 19:39:43 -0500

    Update script.js

--- POODSD/Signed-In/script.js
+++ POODSD/Signed-In/script.js
@@ -1,22 +1,3 @@
-function editContact(id){
-    document.getElementById("editButton"+id).style.display = "none";
-    document.getElementById("saveEditButton"+id).style.display = "inline-block";
-    let firstName = document.getElementById("FirstName" + id);
-    let lastName = document.getElementById("LastName" + id);
-    let email = document.getElementById("EmailAddress" + id);
-    let phone = document.getElementById("PhoneNumber" + id);
-
-    let name_data = firstName.innerText;
-    let namel_data = lastName.innerText;
-    let email_data = email.innerText;
-    let phone_data = phone.innerText;
-
-    firstName.innerHTML = "<input class = 'input-box' type='text' id='nameFirst" + id + "' value='" + name_data + "'>";
-    lastName.innerHTML = "<input class = 'input-box' type='text' id='nameLast" + id + "' value='" + namel_data + "'>";
-    email.innerHTML = "<input class = 'input-box' type='text' id='emailaddress" + id + "' value='" + email_data + "'>";
-    phone.innerHTML = "<input class = 'input-box' type='text' id='phonenumber" + id + "' value='" + phone_data + "'>";
-
-};
 
 document.addEventListener('DOMContentLoaded', function() {
     const contactForm = document.querySelector('.contact-form');
