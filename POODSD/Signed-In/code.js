c6a51e8e9774bc82dc2e0cea205b16f457b9ef14
Gino Benitez <benitezgp81@gmail.com>
2024-02-06 13:03:04 -0500

    Update code.js

--- POODSD/Signed-In/code.js
+++ POODSD/Signed-In/code.js
@@ -174,7 +174,7 @@ function addContact()
 	let newEmail = document.getElementById("newEmail").value;
 	document.getElementById("addContactResult").innerHTML = "";
 	
-	formattedNumber = newNumber.replace('-',""); 
+	let formattedNumber = newNumber.replace('-',""); 
 	newNumber= formatedNumber.split(" ").join("");
 	let tmp = {firstName:newFirst,lastName:newLast,phone:newNumber,email:newEmail,userId:userId};
 	let jsonPayload = JSON.stringify( tmp );
