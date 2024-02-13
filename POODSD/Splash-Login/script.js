6ab16cc0562a0fd44ab8263224ebc2d4d0bedcb6
Gino Benitez <benitezgp81@gmail.com>
2024-02-12 20:27:04 -0500

    styling

--- POODSD/Splash-Login/script.js
+++ POODSD/Splash-Login/script.js
@@ -5,24 +5,60 @@ const signupLink = document.querySelector('.signup-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const closeLogin = document.querySelector('.close-btn');
 const closeRegister = document.querySelector('[type="register-close"]');
-
+const aboutBtn = document.querySelector('.btnAbout-popup');
+const home = document.querySelector('.btnHome-popup');
 const loadCircle = document.querySelector('.load-circle');
 const loadOval = document.querySelector('.load-oval');
 
 // BEGIN EDIT FOR SPLASH TEXT
 document.addEventListener('DOMContentLoaded', function() {
     const welcomeText = document.querySelector('.welcome-text');
-
+    const welcomeBottom = document.querySelector('.welcome-bottom-text'); 
+    const about = document.querySelector('.about-box'); 
     btnPopup.addEventListener('click', () => {
         welcomeText.style.display = 'none';
+        welcomeBottom.style.display = 'none';
+        about.style.display = 'none';
     });
 
     closeLogin.addEventListener('click', () => {
         welcomeText.style.display = 'block';
+        welcomeBottom.style.display = 'block';
+        about.style.display = 'none';
     });
+   
 
     closeRegister.addEventListener('click', () => {
         welcomeText.style.display = 'block';
+        welcomeBottom.style.display = 'block';
+        about.style.display = 'none';
+    });
+
+    home.addEventListener('click', () => {
+        welcomeText.style.display = 'block';
+        welcomeBottom.style.display = 'block';
+        about.style.display = 'none';
+        wrapper.classList.remove('active-popup');
+        wrapper.classList.remove('active');
+        loadCircle.style.display = 'none';
+        loadOval.style.display = 'none';
+    
+        wrapper.classList.remove('active-popup');
+        loadCircle.style.display = 'none';
+    });
+
+   aboutBtn.addEventListener('click', () => {
+    welcomeText.style.display = 'none';
+    welcomeBottom.style.display = 'none';
+    about.style.display = 'block';
+
+    wrapper.classList.remove('active-popup');
+    wrapper.classList.remove('active');
+    loadCircle.style.display = 'none';
+    loadOval.style.display = 'none';
+
+    wrapper.classList.remove('active-popup');
+    loadCircle.style.display = 'none';
     });
 });
 // END EDIT FOR SPLASH TEXT
